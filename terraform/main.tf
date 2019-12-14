574305e12fe1253b9b1b36627bbfd53d00805850
subz10 <30402230+subz10@users.noreply.github.com>
2019-12-15 00:09:43 +0530

    Delete main.tf

--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    access_key = "ASIA6H6A2XPC6YKYPAEF"
-    secret_key = "VJ8RxtGFhdXtQ2sDQAU1sMXvPrKBKXJLkCdPhJS+"
-    region = "us-east-1"  # e.g. eu-west-1
-}
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-  acl    = "private"
-}
